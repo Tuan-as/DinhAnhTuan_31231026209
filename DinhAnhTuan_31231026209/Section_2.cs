469988edc78131218e03a4fe7711842c1e297b88
Administrator <Administrator@N301PC29>
2024-10-11 23:23:24 +0700

    Add project files.

---
+++ DinhAnhTuan_31231026209/Section_2.cs
@@ -0,0 +1,138 @@
+﻿using Microsoft.VisualBasic;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DinhAnhTuan_31231026209
+{
+    internal class Section_2
+    {
+
+        public static void Main()
+        {
+            //Q1();
+            //Q2();
+            //Q3();
+            //Q4();
+            //Q5();
+            //Q6(); chua lam duoc
+            Q7();
+            Q8();
+            Q9();
+            Q10();
+
+
+        }
+
+        /// <summary>
+        /// 1.to Add/Sum Two number
+        /// </summary>
+        public static void Q1()
+        {
+            Console.Write("Hay nhap so thu nhat: ");
+            int a= int.Parse(Console.ReadLine());
+            Console.Write("Hay nhap so thu hai: ");
+            int b= int.Parse(Console.ReadLine());
+            int sum = a + b;
+            Console.WriteLine($"Tong cua {a} va {b} la {sum}");
+        }
+        /// <summary>
+        /// 2.to Swap Values of Two Variable
+        /// </summary>
+        public static void Q2()
+        {
+            Console.Write("Hay nhap so a: ");
+            int a = int.Parse(Console.ReadLine());
+            Console.Write("Hay nhap so b: ");
+            int b = int.Parse(Console.ReadLine());
+            int c = a;
+            a = b; b = c;
+            Console.WriteLine($"a bay gio la {a}, b bay gio la {b}");
+        }
+        /// <summary>
+        /// 3.to Multiply two floating point number
+        /// </summary>
+        public static void Q3()
+        {
+            Console.Write("Hay nhap so thap phan thu nhat: ");
+            double a = double.Parse(Console.ReadLine());
+            Console.Write("Hay nhap so thap phan thu hai: ");
+            double b = double.Parse(Console.ReadLine());
+            double tich = a * b;
+            Console.WriteLine($"tich cua hai so thap phan {a}, {b} la {tich}");
+        }
+        /// <summary>
+        /// 4.to convert feet to meter
+        /// </summary>
+        public static void Q4()
+        {
+            Console.Write("Do dai theo don vi feet: ");
+            double a = double.Parse(Console.ReadLine());
+            double doidonvi = 0.305;
+            double meter = a * doidonvi;
+            Console.WriteLine($"Do dai theo don vi meter: {meter}");
+        }
+        /// <summary>
+        /// 5.to convert Celcius to Fahrenheit and vice versa
+        /// </summary>
+        public static void Q5()
+        {
+            Console.Write("Dien C de doi tu Celcius thanh Fahrenheit, Dien F de doi tu Fahrenheit thanh Celcius: ");
+            string donvi = Console.ReadLine();
+            if (donvi == "C")
+            {
+                Console.Write("Nhap nhiet do Celcius: ");
+                double nhietdoC = double.Parse(Console.ReadLine());
+                double nhietdoF = nhietdoC * (9 / 5) + 32;
+                Console.WriteLine($"Nhiet do Fahrenheit la: {nhietdoF}");
+            }
+            else
+            {
+                Console.Write("Nhap nhiet do Fahrenheit: ");
+                double nhietdoF = double.Parse(Console.ReadLine());
+                double nhietdoC = (nhietdoF - 32) / (9 / 5);
+                Console.WriteLine($"Nhiet do Celcius la: {nhietdoC}");
+            }
+        }
+        /// <summary>
+        /// 6.to find the size of data type
+        /// </summary>
+        public static void Q6()
+        {
+            int x= sizeof(int);
+            Console.WriteLine($"Do lon cua kieu int la: {x}");
+        }
+        /// <summary>
+        /// 7.to print ASCII Value
+        /// </summary>
+        public static void Q7()
+        {
+            Console.WriteLine("Nhap ki tu: ");
+            char kitu = Console.ReadKey
+        }
+        /// <summary>
+        /// 8.to Calculate Area of circle
+        /// </summary>
+        public static void Q8()
+        {
+
+        }
+        /// <summary>
+        /// 9.to calculate Area of Sphere
+        /// </summary>
+        public static void Q9()
+        {
+
+        }
+        /// <summary>
+        /// 10.to convert days to years, weeks and days
+        /// </summary>
+        public static void Q10()
+        {
+            Console.WriteLine("Nhap so ngay:");
+            int songay= Parse
+        }
+    }
+}
