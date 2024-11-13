c13efe4966438cedf910db629e6eeee76295bfda
31231026312@ueh.edu.vn <31231026312@ueh.edu.vn>
2024-11-13 23:51:18 +0700

    gv

--- DinhAnhTuan_31231026209/session_6.cs
+++ DinhAnhTuan_31231026209/session_6.cs
@@ -8,6 +8,8 @@ using System.Text;
 using System.Threading.Tasks;
 using static System.Runtime.InteropServices.JavaScript.JSType;
 using System.Xml.Linq;
+using System.Runtime;
+using System.Reflection.Metadata.Ecma335;
 
 namespace DinhAnhTuan_31231026209
 {
@@ -30,24 +32,24 @@ namespace DinhAnhTuan_31231026209
         public static void taomang(int[] a)
         {
             Random random = new Random();
-            for (int i = 0; i < a.Length; i ++)
+            for (int i = 0; i < a.Length; i++)
             {
                 a[i] = random.Next(100);
             }
-           
+
         }
         public static void inmang(int[] a)
-        { 
+        {
             foreach (int i in a)
                 Console.Write($"{i} ");
             Console.WriteLine();
         }
         public static void tanggiatri(int[] a, int b)
         {
-            for (int i = 0;i < a.Length;i ++)
+            for (int i = 0; i < a.Length; i++)
             {
                 a[i] += b;
-            }    
+            }
         }
         public static double tinhtong(int[] a)
         {
@@ -66,10 +68,12 @@ namespace DinhAnhTuan_31231026209
         }
         public static int min(int[] a)
         {
-            int min = a[1];
-            foreach (int i in a)
-                if (i < min)
+            int min = a[0];
+            for (int i = 0;i < a.Length;i++)
+                if (a[i] < min)
+                {
                     min = a[i];
+                }
             return min;
         }
         public static void Ex1()
@@ -81,7 +85,7 @@ namespace DinhAnhTuan_31231026209
             inmang(a);
             tanggiatri(a, 2);
             inmang(a);
-            double x=tinhtong(a);
+            double x = tinhtong(a);
             Console.WriteLine($"tong cua mang la: {x}");
             trungbinh(a);
             int y = min(a);
@@ -104,11 +108,10 @@ namespace DinhAnhTuan_31231026209
             {
                 if (a[i] == giatri)
                 {
-                    Console.WriteLine("Co gia tri nay trong mang");
+                    
                     return true;
                 }
             }
-            Console.WriteLine("Khong co giá tri nay trong mang");
             return false;
         }
         public static int vitrigiatri(int[] a, int giatri)
@@ -125,22 +128,194 @@ namespace DinhAnhTuan_31231026209
             vitri = -1;
             return vitri;
         }
+        public static int[] delai1gttrunglap(int[] a, int b)
+        {
+            int count = 0, j = 0;
+            for (int i = 0; i < a.Length; i++)
+                if (a[i] == b)
+                {
+                    count++;
+                }
+            int[] x = new int[a.Length - count+1];
+            int dem = 0;
+            for (int i = 0; i < a.Length; i++)
+            {
+                if ((a[i] == b) && (dem ==0))
+                {
+                    dem++;
+                    x[i - j] = a[i];
+                    continue;
+                }
+                else if (a[i]==b)
+                {
+                    j++;
+                }
+                else x[i - j] = a[i];
+            }
+            return x;
+        }
+        public static int[] loaibophantu(int[] a, int b)
+        {
+            int count=0 ,j = 0;
+            for (int i = 0; i < a.Length; i++)
+                if (a[i] == b)
+                {
+                    count++;
+                }
+            int[] x = new int[a.Length -count];
+            for (int i = 0;i < a.Length;i++)
+            {
+                if (a[i] == b)
+                {
+                    j++;
+                }    
+                else x[i-j] = a[i];
+            }
+            return x;
+        }
+        public static int max(int[] a)
+        {
+            int max = a[0];
+            for (int i = 0; i < a.Length; i++)
+                if (a[i] > max)
+                { 
+                    max = a[i];
+                }
+            return max;
+        }
+        public static void swap( ref int a, ref int b)
+        {
+            int c = a; a = b; b = c;
+        }
+        public static void daonguocmang(int[] a)
+        {
+            
+            for (int i = 0; i <= (a.Length / 2 - 1); i++)
+            {
+                swap(ref a[i], ref a[a.Length - 1 - i]);
+            }    
+        }
+        public static bool cotrunglapkhong(int[] a, int b)
+        {
+            int count = 0;
+            for (int i = 0; i < a.Length; i++)
+            {
+                if (b == a[i])
+                    count++;
+                if (count >= 2)
+                    return true;
+            }
+            return false;
+        }
+        public static int sogiatritrunglap(int[] a)
+        {
+            int count = 0;
+            int[] x = a;
+            int i = 0;
+            do
+            {
+                i = 0;
+                if (x.Length == 0)
+                    break;
+                if (cotrunglapkhong(x, x[i]) == true)
+                {
+                    count++;
+                    x = loaibophantu(x, x[i]);
+                }
+                else
+                {
+                    x = loaibophantu(x, x[i]);
+                }
+
+            }
+            while (true);
+            return count;
+        }
+        public static int[] giatritrunglap(int[] a)
+        {
+            if (sogiatritrunglap(a) == 0)
+            {
+                Console.WriteLine("Mang khong co gia tri trung lap!");
+                return new int[0];
+            }
+            int[] b = new int[sogiatritrunglap(a)]; int z = 0;
+            int[] x = a; int i = 0;
+            do
+            {
+                i = 0;
+                if (x.Length == 0)
+                    break;
+                if (cotrunglapkhong(x, x[i]) == true)
+                {
+                    b[z] = x[i];
+                    z++;
+                    x = loaibophantu(x, x[i]);
+                }
+                else
+                {
+                    x = loaibophantu(x, x[i]);
+                }
+
+            }
+            while (true);
+            return b;
+        }
+        public static int[] loaibotrunglap(int[] a)
+        {
+            int[] b = giatritrunglap(a);
+            for (int i = 0;i< b.Length; i++)
+            {
+                a = delai1gttrunglap(a, b[i]);
+            }
+            return a;
+        }
         public static void Ex2()
         {
             Console.Write("Nhap so phan tu cua mang: ");
             int n = int.Parse(Console.ReadLine());
             int[] a = new int[n];
             taomang(a);
-            trungbinh(a); //1.
+            trungbinh(a);                                                           //1.
             inmang(a);
-            Console.Write("Nhap gia tri muon kiem tra: "); //2.
+            Console.Write("Nhap gia tri muon kiem tra: ");                          //2.
             int gt= int.Parse(Console.ReadLine());
-            cogiatrikhong(a, gt);
-            int vitri = vitrigiatri(a, gt); //3.
-            if (vitri == -1)
-                Console.WriteLine("Khong co gai tri nay trong mang");
-            else { Console.WriteLine($"Gia tri nay o vi tri {vitri} cua mang"); }
-
+            int vitri = vitrigiatri(a, gt);                                         //3.
+            if (cogiatrikhong(a, gt) == true)
+            {
+                Console.WriteLine("Co gia tri nay trong mang");
+                Console.WriteLine($"Gia tri nay o vi tri {vitri} cua mang"); 
+            }
+            else Console.WriteLine("Khong co giá tri nay trong mang");
+            Console.Write("Chon so ma ban muon loai bo: ");                         //4.
+            int bo = int.Parse(Console.ReadLine());
+            a = loaibophantu(a, bo);
+            Console.Write("Mang moi la: "); inmang(a);
+            int gtnn = min(a);                                                      //5.
+            int gtln = max(a);
+            Console.WriteLine(@$"Gia tri nho nhat cua mang la: {gtnn}
+Gia tri lon nhat cua mang la: {gtln}");
+            Console.WriteLine(@"Ban co muon dao nguoc mang khong?                  
+Neu muon, nhan 'Y'; neu khong, nhan phim bat ky: ");                                //6.
+            char option = char.Parse(Console.ReadLine());                           
+            if (option == 'Y' || option == 'y')
+            {
+                daonguocmang(a);
+                inmang(a);
+            }
+            int[] b = giatritrunglap(a);                                            //7.
+            if (b.Length == 0)
+            {
+                Console.WriteLine("");
+            }    
+            else Console.Write("Cac gia tri bi trung lap la: "); inmang(b);
+            Console.WriteLine(@"Ban co muon loai bo cac gia tri trung lap khong? 
+Neu muon, nhan 'Y'; Neu khong, nhan phim bat ky: ");
+            char luachon = char.Parse(Console.ReadLine());                           //8.
+            if (luachon == 'Y' || luachon == 'y')
+            {
+                a=loaibotrunglap(a);
+                inmang(a);
+            }
         }
     }
 }
